9889f97f1d73951ae8ad793abbd7c1be30dc0cf0
Nehmat <gere0018@algonquinlive.com>
2015-02-09 17:30:17 -0500

    fixed part B

--- js/script.js
+++ js/script.js
@@ -4,19 +4,22 @@ document.addEventListener("DOMContentLoaded", function(){
 //PART A
 //Build 6 paragraphs with filler text inside #main. When you click on the paragraph
   //the index number of the paragraph becomes the innerHTML. Eg:  1, 2, 3, 4, 5, 6
-  (function clickPara ( ){
-  for(var p=1; p < 6; p++){
+  function clickPara ( ){
+  for(var p=1; p <= 6; p++){
     var para = document.createElement("p");
     para.innerHTML = "I Love JavaScript " + p + " closure at a time";
-    para.addEventListener("click", (function(j){
-      return function( ){
-        alert(j);
+    para.addEventListener("click", (function(j, k){
+    // this function returns a function. We return a function to lock the value of P and para.
+        return function(){
+        k.innerHTML = j;
       }
-    })(p) );
+    }) (p, para));// We need to use "immediately invoked function expression" so we can capture the value of p and para at the time it is clicked.
+
     var mainDiv =document.getElementById("main");
     mainDiv.appendChild(para);
   }
-})();
+}
+    clickPara();
   //PART B
   //after the user clicks on the heading inside #second, start adding list items to the
   //unordered list #theList. Use a for loop and a setTimeout. The delay for the first list item to appear
@@ -24,19 +27,19 @@ document.addEventListener("DOMContentLoaded", function(){
   //Add a total of five list items to the ul.
     (function addItem ( ){
     var H = document.querySelector("#second h2");
-    H.addEventListener("click", (function(a){
-        var list = document.querySelector("#theList");
-        var item = document.createElement("li");
-        for(var i=1; i < 6; i++){
-            setTimeout(function() {
+    var list = document.querySelector("#theList");
+    for(var i=1; i < 6; i++){
+    var item = document.createElement("li");
+    H.addEventListener("click", (function(i, item){
+        return function( ){
+            window.setTimeout(function() {
                 item.innerHTML = "I am listItem #" + i;
-            }, i)
+                list.appendChild(item);
+            }, (i*1000));
         }
-      return function( ){
-        alert(a);
-      }
-    })(i) );
-    var UL =document.querySelector("#second ul");
-    UL.appendChild(item);
+    })(i, item) );
+    }
+
+   })();
 
 });
