ea06d8025b0d6bc6c3111c2c9adaa40fc48c5349
Nehmat <gere0018@algonquinlive.com>
2015-02-10 13:48:16 -0500

    fixed comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 
-//Using CLOSURES to solve two problems
 document.addEventListener("DOMContentLoaded", function(){
-//PART A
-//Build 6 paragraphs with filler text inside #main. When you click on the paragraph
-  //the index number of the paragraph becomes the innerHTML. Eg:  1, 2, 3, 4, 5, 6
+/*PART A
+Build 6 paragraphs with filler text inside #main. When you click on the paragraph the index number of the paragraph becomes the innerHTML. Eg:  1, 2, 3, 4, 5, 6*/
   function clickPara ( ){
   for(var p=1; p <= 6; p++){
     var para = document.createElement("p");
     para.innerHTML = "I Love JavaScript " + p + " closure at a time";
     para.addEventListener("click", (function(j, k){
-    // this function returns a function. We return a function to lock the value of P and para.
+    // This function returns a function. We return a function to lock the value of P and para.
         return function(){
         k.innerHTML = j;
       }
@@ -19,12 +17,11 @@ document.addEventListener("DOMContentLoaded", function(){
     mainDiv.appendChild(para);
   }
 }
-    clickPara();
-  //PART B
-  //after the user clicks on the heading inside #second, start adding list items to the
-  //unordered list #theList. Use a for loop and a setTimeout. The delay for the first list item to appear
-  //is one second, two second delay for the second one, three second delay for the third and so on.
-  //Add a total of five list items to the ul.
+ clickPara();
+
+  /*PART B
+  //after the user clicks on the heading inside #second, start adding list items to the unordered list #theList. Use a for loop and a setTimeout. The delay for the first list item to appear is one second, two second delay for the second one, three second delay for the third and so on. Add a total of five list items to the ul.*/
+
     (function addItem ( ){
     var H = document.querySelector("#second h2");
     var list = document.querySelector("#theList");
